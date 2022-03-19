ec95428fe686743b578c2fe48927824375b0373a
JunYoung Lee <52552781+junlight94@users.noreply.github.com>
2022-03-19 22:05:00 +0900

    finished

--- dynamicAnimator/ViewController.swift
+++ dynamicAnimator/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var outView: UIView!
+    
+    var dynamic = UIDynamicAnimator()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //MARK: refernceView outView로 설정
+        dynamic = UIDynamicAnimator(referenceView: outView)
+        
+        //MARK: 중력 설정
+        let gravity = UIGravityBehavior(items: [orangeView])
+        
+        //MARK: 충돌 설정
+        let collision = UICollisionBehavior(items: [orangeView])
+        //설정된 referenceView 경계와 충돌 설정
+        collision.translatesReferenceBoundsIntoBoundary = true
+        
+        //MARK: 탄성 설정
+        let behavior = UIDynamicItemBehavior(items: [orangeView])
+        // 값이 높을 수록 탄성 up
+        behavior.elasticity = 0.7
+        
+        //MARK: 설정한 애니메이션 추가
+        dynamic.addBehavior(gravity)
+        dynamic.addBehavior(collision)
+        dynamic.addBehavior(behavior)
+        
     }
 
 
